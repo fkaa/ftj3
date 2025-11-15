9bd97295ddef9b05cd745d463e02ee2352c86a92
APirateHat <anton.bjareborn@gmail.com>
2025-11-15 09:58:10 +0100

    Sprite sheets and animationes wip

--- scene/character/monkey/monkey.gd
+++ scene/character/monkey/monkey.gd
@@ -8,6 +8,9 @@ const JUMP_VELOCITY = -300.0
 @export var run_speed: float = 1500.0
 @export var run_slowdown: float = 3000.0
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("run")
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -22,8 +25,23 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x += direction * run_speed * delta
+		print(direction)
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
+		if direction == 1:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, delta * run_slowdown)
+	
+	if !is_on_floor():
+		$AnimatedSprite2D.play("jump")
+	else:
+		if abs(velocity.x) > 0:
+			$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("idle")
 
 	if velocity.x < -max_speed:
 		velocity.x = -max_speed
